f1bd5e7d43b278414436d12022fd29ef921f5078
Julia Schatzmann <julia.schatzmann@students.fhv.at>
2018-03-05 21:01:22 +0100

    initial commit

---
+++ Model/Base/BaseNamedModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Base
+{
+    public class BaseNamedModel
+    {
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Model
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string TelephoneNumber { get; set; }
+
+
+
+
+    }
+}
