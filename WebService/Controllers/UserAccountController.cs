7ff8b7deb389f47c3847ecd4616fd07e081c90bb
Julia Schatzmann <julia.schatzmann@students.fhv.at>
2018-03-15 15:09:41 +0100

    added dapper conifg

--- WebService/Controllers/UserAccountController.cs
+++ WebService/Controllers/UserAccountController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using Database;
+using Model;
 using WebService.Models;
 
 namespace WebService.Controllers
@@ -18,27 +19,74 @@ namespace WebService.Controllers
             this.service = new UserAccountService();
         }
 
-        [HttpGet]
-        public IHttpActionResult Get()
-        {
-            return Ok("YEAH");
-        }
-
         [HttpPost]
-        public IHttpActionResult Login(
-            [FromBody]string username,
-            [FromBody]string password)
+        [ActionName("login")]
+        public IHttpActionResult Login([FromBody] UserAccount userAccount)
         {
             using (IDbConnection connection = base.ConnectionProvider.GetMySqlConnection())
             {
-                bool loginSuccessful = service.Login(connection, username, password);
+                bool loginSuccessful = service.Login(connection, userAccount.UserName, userAccount.Password);
 
                 if (loginSuccessful)
                 {
                     return Ok("Your are logged in.");
                 }
+
                 return BadRequest("Sorry, Login failed.");
             }
         }
+
+        [HttpGet]
+        [ActionName("logout")]
+        public IHttpActionResult Logout(string username)
+        {
+            using (IDbConnection connection = base.ConnectionProvider.GetMySqlConnection())
+            {
+                //TODO
+            }
+
+            return null;
+        }
+
+        //[HttpPost]
+        //[Route("Insert")]
+        //public IHttpActionResult Insert(UserAccount userAccount)
+        //{
+        //    using (IDbConnection connection = base.ConnectionProvider.GetMySqlConnection())
+        //    {
+        //        int result = service.Save(connection, item);
+        //        return Ok(new { id = result });
+        //    }
+        //}
+
+        //[HttpPut]
+        //[Route("Update")]
+        //public IHttpActionResult Update(int id, [FromBody]Tour item)
+        //{
+        //    using (IDbConnection connection = base.ConnectionProvider.GetMySqlConnection())
+        //    {
+        //        Tour oldItem = service.Get(connection, id);
+        //        if (oldItem == null)
+        //        {
+        //            return NotFound();
+        //        }
+
+        //        int result = service.Save(connection, item);
+        //        return Ok(new { id = result });
+        //    }
+        //}
+
+        //[HttpDelete]
+        //[Route("{id}")]
+        //public IHttpActionResult Delete(int id)
+        //{
+        //    using (IDbConnection connection = base.ConnectionProvider.GetMySqlConnection())
+        //    {
+        //        int rows = service.Delete(connection, id);
+        //        return Ok(new { count = rows });
+
+
+        //    }
+        //}
     }
 }
